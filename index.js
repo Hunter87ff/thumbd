26678ccd83629a7f5edf2f4eced61ff93b0dc47d
Hunter Gaming <83773586+Hunter87ff@users.noreply.github.com>
2021-06-28 15:01:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('resources/sw.js')
+    .register(sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
